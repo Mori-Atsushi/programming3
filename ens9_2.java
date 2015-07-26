414b0e3bb3b3592afb6c38afef3e81aa18445cde
Mori Atsushi <e1237@s.akashi.ac.jp>
2015-07-26 21:44:23 +0900

    演習9-1 完了

---
+++ ens9_2.java
@@ -0,0 +1,18 @@
+public class ens9_2 {
+	public static void main(String[] args) {
+		TurtleFrame f = new TurtleFrame();
+		HTurtle m;
+		
+		if(args.length > 0)
+			m = new Tensen();
+		else
+			m = new HTurtle();
+
+		f.add(m);
+		m.fd(100);
+		m.rt(90);
+		m.up(); m.fd(100); m.down();
+		m.lt(90);
+		m.polygon(5, 50);
+	}
+}
