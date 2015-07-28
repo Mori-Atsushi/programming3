350f4270a2c785c3d4b183286d462e936ccbb61e
Mori Atsushi <e1237@s.akashi.ac.jp>
2015-07-28 10:38:35 +0900

    演習11-1 完了

---
+++ ens11_1.java
@@ -0,0 +1,39 @@
+class SuperClass {
+	String field = "superField";
+
+	String method() {
+		return "superMethod";
+	}
+	String getField() {
+		return field;
+	}
+}
+
+class SubClass extends SuperClass {
+	String field = "subField";
+
+	String method() {
+		return "subMethod";
+	}
+}
+
+class ens11_1 {
+	public static void main(String[] args) {
+		SuperClass clz1 = new SuperClass();
+		System.out.println(clz1.field);
+		System.out.println(clz1.method());
+		System.out.println(clz1.getField());
+		System.out.println();
+
+		SubClass clz2 = new SubClass();
+		System.out.println(clz2.field);
+		System.out.println(clz2.method());
+		System.out.println(clz2.getField());
+		System.out.println();
+
+		SuperClass clz3 = new SubClass();
+		System.out.println(clz3.field);
+		System.out.println(clz3.method());
+		System.out.println(clz3.getField());
+	}
+}
